15487de63dd46e48bfe22f6e3666da3ba9135c03
sameer rasa <crazzywizard@gmail.com>
2025-08-25 18:03:43 -0400

    fixing deployment errors

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,12 +1,1 @@
-declare module 'minimatch' {
-  const minimatch: {
-    (pattern: string, str: string, options?: object): boolean;
-    filter(pattern: string): (str: string) => boolean;
-  };
-  export = minimatch;
-}
-
-declare module 'next-pwa' {
-  const withPWA: (config: object) => object;
-  export default withPWA;
-}
+ 
